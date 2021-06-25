da47550d89a98f46512bf39670a62b5475ad24c2
Praveen Ilangovan <praveen.ilangovan@gmail.com>
2021-06-25 01:27:47 +0100

    Added Haas Cascade face detection function

--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,12 @@ import os
 
 NAME = "GOT Face Recognizer"
 
+# Directories
 SRC_DIR = os.path.dirname(__file__)
 RESOURCES_DIR = os.path.join(os.path.dirname(SRC_DIR), "resources")
+CLASSIFIERS_DIR = os.path.join(RESOURCES_DIR, "classifiers")
 TRAINING_DATA_DIR = os.path.join(RESOURCES_DIR, "training")
+
+# Classifiers
+HAAR_CASCADE = "haar_cascade"
+AVAILABLE_CLASSIFIERS = [HAAR_CASCADE]
--- src/cv_utils.py
+++ src/cv_utils.py
@@ -1,4 +1,6 @@
-from typing import Optional
+import os
+from typing import List, Optional
+
 import cv2 as cv #type: ignore
 import numpy as np
 
@@ -34,10 +36,106 @@ def read_image(imgpath: str) -> Optional[np.ndarray]:
 #-----------------------------------------------------------------------------#
 def convert_to_grayscale(img: Optional[np.ndarray]) -> Optional[np.ndarray]:
     """ Convert the image's colour space to grayscale
+
+    Args:
+        img numpy.ndarray: Image in an numpy array format
+
+    rtype:
+        numpy.ndarray
+
+    Returns:
+        A numpy array
     """
     if img is None:
-        return
+        return None
     return cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+def resize_image(img: Optional[np.ndarray], width: int= -1,
+                 height: int = -1, inter = cv.INTER_AREA) -> Optional[np.ndarray]:
+    """ Resize the image by keeping its aspect ratio
+
+    Args:
+        img numpy.ndarray: Image in an numpy array format
+        width int: Width to resize to
+        height int: Height to resize to
+        inter: opencv interpolation to use.
+
+    rtype:
+        numpy.ndarray
+
+    Returns:
+        A numpy array
+    """
+    if img is None:
+        return None
+
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = img.shape[:2]
+
+    if width == -1 and height == -1:
+        return img
+
+    if width == -1:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv.resize(img, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
+def get_faces(img: Optional[np.ndarray], classifier: str= constants.HAAR_CASCADE) -> List:
+    """ Detect faces in the image using the specified classifier
+
+    Args:
+        img numpy.ndarray: Image in an numpy array format
+        classifier str: Classifier to use
+
+    rtype:
+        List
+
+    Returns:
+        A list of rectangle coordinates: [[x,y,w,h]]
+    """
+    # convert it to gray
+    gray = convert_to_grayscale(img)
+
+    # Detect faces
+    cascade_classifier = get_classifer(classifier)
+    if cascade_classifier is None:
+        return []
+    return cascade_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+
+#-----------------------------------------------------------------------------#
+#
+# Classifiers
+#
+#-----------------------------------------------------------------------------#
+def get_classifer(classifier: str) -> Optional[cv.CascadeClassifier]:
+    """ Return the cascade classifier
+    """
+    if classifier == constants.HAAR_CASCADE:
+        return get_haar_cascade_classifier()
+    return None
+
+def get_haar_cascade_classifier() -> cv.CascadeClassifier:
+    """ Return haar cascade classifier
+
+    rtype:
+        cv.CascadeClassifier
+    """
+    cascade_file = os.path.join(constants.CLASSIFIERS_DIR, "haar_face.xml")
+    return cv.CascadeClassifier(cascade_file)
 
 #-----------------------------------------------------------------------------#
 #
@@ -70,4 +168,11 @@ def display_image(imgpath: str, window_name: str= constants.NAME, wait: int= 0) 
             Defaults to 0 and this makes sure the window shows until the
             user closes it.
     """
-    show_image(read_image(imgpath))
+    img = read_image(imgpath)
+    img = resize_image(img, height=250)
+    rects = get_faces(img)
+
+    for x,y,w,h in rects:
+        cv.rectangle(img, (x,y), (x+w, y+h), (0,128,0), thickness=2)
+
+    show_image(img)
